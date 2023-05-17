dac33b89d1dbd48594a7b54e84ba242f223ca98d
Jakub Pietraszek <yaaqbp@gmail.com>
2023-05-17 12:48:28 +0200

    prediction threshold added:

--- image_classification/ml_models.py
+++ image_classification/ml_models.py
@@ -1,5 +1,7 @@
 from torch import load as torch_load
 from torchvision import transforms
+from torch.nn import Softmax
+import numpy as np
 from PIL import Image
 import io
 from image_classification.descriptions import descriptions
@@ -18,11 +20,13 @@ class clf():
         self.lesion_type_dict = descriptions
 
     def predict(self, image_bytes):
-        self.classes = ['akiec', 'bcc', 'bkl', 'df', 'nv', 'vasc','mel']
+        self.classes = ['akiec', 'bcc', 'bkl', 'df', 'nv', 'vasc','mel', 'unkown']
         img = Image.open(io.BytesIO(image_bytes))
         tensor_img = self.basic_transform(img).unsqueeze(0)
         outputs = self.model(tensor_img)
-        prediction = outputs.max(1, keepdim=True)[1]
+        softmax = Softmax(dim=1)
+        probabilities = softmax(outputs).data.numpy()[0]
+        prediction = np.argmax(probabilities) if max(probabilities) >0.8 else -1
         disease = self.lesion_type_dict.get(self.classes[int(prediction)], {
         })
     
